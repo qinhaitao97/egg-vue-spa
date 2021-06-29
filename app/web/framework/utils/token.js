da9b3eee6a1899dde57327e60fe215dce95ef0c7
qinhaitao97 <870554179@qq.com>
2021-06-29 10:11:18 +0800

    feat: 封装token操作

---
+++ app/web/framework/utils/token.js
@@ -0,0 +1,29 @@
+/** 封装 token 操作
+ * @user qinhaitao
+ */
+ 'use strict'
+
+ import storage from './storage'
+
+ export default {
+   getToken() {
+     return storage.getItem('local', 'token')
+   },
+
+   hasToken() {
+     const token = storage.getItem('local', 'token') === ''
+     return (token === '') || (token === undefined) ? false : true
+   },
+
+   /** 设置 token
+    * @param {String} token
+    */
+   setToken(token) {
+     storage.setItem('local', 'token', token)
+   },
+
+   /**　清除 token */
+   clearToken() {
+     storage.clearItem('local', 'token')
+   },
+ }
